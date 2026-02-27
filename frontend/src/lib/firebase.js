aacd89bfe50276aecace9c5f4a79a1cf5923f0af
shubham <gavadeshubham2004@gmail.com>
2026-02-27 12:48:03 +0530

    modified:   frontend/src/lib/firebase.js

--- frontend/src/lib/firebase.js
+++ frontend/src/lib/firebase.js
@@ -3,7 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCO9_geqZzJuPDpumDShDZpyVPYKzZnrS4",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "crime-detection-system-734c6.firebaseapp.com",
   projectId: "crime-detection-system-734c6",
   storageBucket: "crime-detection-system-734c6.firebasestorage.app",
